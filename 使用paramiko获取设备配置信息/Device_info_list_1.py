c6e05014c2dc803e4c3dd3588f939a7ad5f647ce
surenx126 <75774389+surenx126@users.noreply.github.com>
2021-01-22 19:22:48 +0800

    Create Device_info_list_1.py

---
+++ 使用paramiko获取设备配置信息/Device_info_list_1.py
@@ -0,0 +1,15 @@
+device_info_list = \
+[
+    {
+        'hostname' : 'R11',
+        'username' : 'admin',
+        'password' : 'admin',
+        'ip_address' : '192.168.152.11'
+    },
+    {
+        'hostname' : 'R12',
+        'username' : 'admin',
+        'password' : 'admin',
+        'ip_address' : '192.168.152.12'
+    }
+]
